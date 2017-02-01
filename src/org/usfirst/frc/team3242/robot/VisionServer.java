9c0bc099a4ebf1000470fbc7ba5a164e23eb5ac2
Tyler Burt <tyler.burtt@gmail.com>
2017-02-01 16:51:29 -0500

    implement built in filters for vision

--- src/org/usfirst/frc/team3242/robot/VisionServer.java
+++ src/org/usfirst/frc/team3242/robot/VisionServer.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3242.robot;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.filters.LinearDigitalFilter;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class VisionServer {
@@ -10,11 +11,14 @@ public class VisionServer {
 	private boolean found;
 	private double x;
 	private double y;
-	public final VisionCoordinate xCoord;
-	public final VisionCoordinate yCoord;
-	private Timer timer;
-	//30 times per second (camera framerate)
-	private final double updateTime = 0.0333333333;
+	public final VisionCoordinate xCoordRaw;
+	public final VisionCoordinate yCoordRaw;
+	public final LinearDigitalFilter xFiltered;
+	public final LinearDigitalFilter yFiltered;
+	private Timer timeoutTimer;
+	private Timer updateTimer;
+	private final double updateTime = 0.0333333333;//30 times per second (camera framerate)
+	private final double timeoutTime = 2;
 	
 	/**
 	 * retrieves coordinate data from raspberry pi over network tables
@@ -25,27 +29,42 @@ public class VisionServer {
 		found = false;
 		x = -1;
 		y = -1;
-		xCoord = new VisionCoordinate(true);
-		yCoord = new VisionCoordinate(false);
-		timer = new Timer();
-		timer.start();
+		xCoordRaw = new VisionCoordinate(true);
+		yCoordRaw = new VisionCoordinate(false);
+		//15 taps should be half a second
+		xFiltered = LinearDigitalFilter.movingAverage(xCoordRaw, 15);
+		yFiltered = LinearDigitalFilter.movingAverage(yCoordRaw, 15);
+		timeoutTimer = new Timer();
+		timeoutTimer.start();
+		updateTimer = new Timer();
+		updateTimer.start();
 	}
+	
+	public boolean targetFound(){
+		return found;
+	}
+	
 	
 	/**
 	 * @return if a target has been identified
 	 */
-	public boolean update(){
+	private boolean update(){
 		found = table.getBoolean("found", false);
     	if(found){
         	x = table.getNumber("visionX", -1);
     		y = table.getNumber("visionY", -1);
     	}
-		timer.reset();
+		updateTimer.reset();
+		if(timeoutTimer.get() > timeoutTime){
+			xFiltered.reset();
+			yFiltered.reset();
+		}
+		timeoutTimer.reset();
     	return found;
 	}
 	
 	private void checkTime(){
-		if(timer.get() >= updateTime){
+		if(updateTimer.get() >= updateTime){
 			update();
 		}
 	}
@@ -77,7 +96,6 @@ public class VisionServer {
 		@Override
 		public double pidGet() {
 			if(isX){
-				checkTime();
 				return x;
 			}else{
 				return y;
