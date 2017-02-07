f0c5baa15c72707cf87bf537022f59e7f1e57363
BrandonWojo <brandon.wojtaszak@gmail.com>
2017-02-07 17:12:41 -0500

    Update BallPickup.java

--- src/org/usfirst/frc/team3242/robot/BallPickup.java
+++ src/org/usfirst/frc/team3242/robot/BallPickup.java
@@ -4,13 +4,18 @@ protected class BallPickup{
 	private CANTalon elevator;
 	
 	public BallPickup(CANTalon pickup, CANTalon elevator){
-			this.pickup = pickup;
-			this.elevator = elevator;
+		this.pickup = pickup;
+		this.elevator = elevator;
 	}
 	
 	public void pickupBall(){
-			pickup.set(1));
-			elevator.set(1)); //Need to test speeds
+		pickup.set(1);
+		elevator.set(1); //Need to test speeds
+	}
+	
+	public void stopPickup(){
+		pickup.set(0);
+		elevator.set(0);
 	}
 }
 
