76ac5840d0274073a0d2138348e2b6ae64be9886
BrandonWojo <brandon.wojtaszak@gmail.com>
2017-02-07 16:58:35 -0500

    Add files via upload
    simple ball pickup class

---
+++ src/org/usfirst/frc/team3242/robot/BallPickup.java
@@ -0,0 +1,16 @@
+protected class BallPickup{
+	
+	private CANTalon pickup;
+	private CANTalon elevator;
+	
+	public BallPickup(CANTalon pickup, CANTalon elevator){
+			this.pickup = pickup;
+			this.elevator = elevator;
+	}
+	
+	public void pickupBall(){
+			pickup.set(1));
+			elevator.set(1)); //Need to test speeds
+	}
+}
+
