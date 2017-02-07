c883915b0d1270a327cb4f7a2f210b8d9362889d
BrandonWojo <brandon.wojtaszak@gmail.com>
2017-02-07 17:16:06 -0500

    Update BallPickup.java

--- src/org/usfirst/frc/team3242/robot/BallPickup.java
+++ src/org/usfirst/frc/team3242/robot/BallPickup.java
@@ -2,20 +2,28 @@ protected class BallPickup{
 	
 	private CANTalon pickup;
 	private CANTalon elevator;
+	private boolean isEnabled;
 	
 	public BallPickup(CANTalon pickup, CANTalon elevator){
 		this.pickup = pickup;
 		this.elevator = elevator;
+		isEnabled = false;
+	}
+	
+	public boolean isEnabled(){
+		return isEnabled;
 	}
 	
 	public void pickupBall(){
 		pickup.set(1);
 		elevator.set(1); //Need to test speeds
+		isEnabled = true;
 	}
 	
 	public void stopPickup(){
 		pickup.set(0);
 		elevator.set(0);
+		isEnabled = false;
 	}
 }
 
